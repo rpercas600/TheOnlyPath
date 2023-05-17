c6dcc39809b7d9f68b7e00e42c58d58c502caebc
rpercas600 <rpercas600@iesmartinezm.es>
2023-05-17 21:09:14 +0200

    movement with animations done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,18 +4,57 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Animator anim;
 
-    public float moveSpeed = 4.0f;
-    public Transform movePoint;
-    // Start is called before the first frame update
-    void Start()
+    public float moveSpeed;
+
+    private Rigidbody2D rb;
+
+    private float x;
+    private float y;
+
+    private Vector2 input;
+    private bool moving;
+
+    private void Start()
     {
-        movePoint.parent = null;
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        GetInput();
+        Animate();
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = input * moveSpeed;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void GetInput()
     {
-        
+        x = Input.GetAxisRaw("Horizontal");
+        y = Input.GetAxisRaw("Vertical");
+
+        input = new Vector2(x, y);
+        input.Normalize();
+    }
+    private void Animate()
+    {
+        if(input.magnitude > 0.1f || input.magnitude < -0.1f)
+        {
+            moving = true;
+        }
+        else
+        {
+            moving = false;
+        }
+
+        if(moving)
+        {
+            anim.SetFloat("X", x);
+            anim.SetFloat("Y", y);
+        }
+
+        anim.SetBool("Moving", moving);
     }
 }
